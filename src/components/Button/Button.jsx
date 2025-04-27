d4252eed55e0f3958e5361e15fbf48be0900fffe
demyanyshyn <taras.demyanyshyn@gmail.com>
2025-04-27 14:33:05 +0300

    +

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import s from './Button.module.css';
 import clsx from 'clsx';
 const Button = ({ type, callback, children }) => {
-    const isSubmit = type.toLowerCase() === 'submit';
+    const isSubmit = type?.toLowerCase() === 'submit';
     return (
         <button
             type={isSubmit ? 'submit' : 'button'}
